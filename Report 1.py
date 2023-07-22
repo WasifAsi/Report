a8ebaf33b08fae34bec8fd20325a2a6d03982dea
Wasif Asi <wasifasi01@gmail.com>
2023-07-22 13:50:40 +0530

    new

--- Report 1.py
+++ Report 1.py
@@ -1,6 +1,10 @@
 #Variable
 salary=0
 net_salary=0
+first=0 #
+second=0
+third=0
+tax=0
 
 #Input
 salary=int(input("Ender the Monthly Gross Salary : "))
@@ -10,16 +14,25 @@ if (0<salary<=99999):
     net_salary=salary
     print("Net Salary ",net_salary)
 
-elif(99999<salary<=150000):
-    net_salary=salary-salary*5/100
+elif(100000<=salary<=150000):
+    first=salary-99999
+    tax=first*5/100
+    net_salary=salary-tax
     print("Net Salary ",net_salary)
     
-elif(150000<salary<=250000):
-    net_salary=salary-salary*10/100
+elif(150001<=salary<=250000):
+    first= salary-99999
+    second=first-50001
+    tax=50001*5/100 + second*10/100
+    net_salary=salary-tax
     print("Net Salary ",net_salary)
     
 elif(salary>250000):
-    net_salary= salary-salary*12/100
+    first= salary-99999
+    second=first-50001
+    third=second-100000
+    tax=50001*5/100 + 100000*10/100 + third*12/100
+    net_salary= salary-tax
     print("Net Salary ",net_salary)
 
 else:
